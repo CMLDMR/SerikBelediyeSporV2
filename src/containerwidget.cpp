3b40c6e8c6bfe797f0ecaabf92086d05ce8cd002
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-28 18:02:02 +0300

    add some member functions.

--- src/containerwidget.cpp
+++ src/containerwidget.cpp
@@ -1,6 +1,122 @@
 #include "containerwidget.h"
+#include <random>
 
-ContainerWidget::ContainerWidget()
+ContainerWidget::ContainerWidget(mongocxx::database *db_)
+    :WContainerWidget (),mdb(db_)
 {
 
+    mLeftSpace = "";
+    mRightSpace = "";
+    mTopSpace = "";
+    mBottomSpace = "";
+    mBackGroundImg = "";
+
+
+}
+
+void ContainerWidget::setBackGroundImage(const std::string &imgpath)
+{
+    mBackGroundImg = Style::background::url(imgpath);
+    updateStyle();
+}
+
+void ContainerWidget::setBackGroundColor(const std::string &color)
+{
+    mBackGroundImg = Style::background::color::color(color);
+    updateStyle();
+}
+
+void ContainerWidget::setBackGroundRGB(int red, int green, int blue)
+{
+    mBackGroundImg = Style::background::color::rgb(red,green,blue);
+    updateStyle();
+}
+
+void ContainerWidget::setBackGroundRGBA(int red, int green, int blue, double alpha)
+{
+    mBackGroundImg = Style::background::color::rgba(red,green,blue,alpha);
+    updateStyle();
+}
+
+void ContainerWidget::setBackGroundRGBRandom(int begin, int end)
+{
+    if( begin > end ) begin = end-1;
+    if( begin < 0 ) begin = 0;
+    if( end < begin ) end = begin + 1;
+    if( end > 255 ) end = 255;
+
+    mBackGroundImg = Style::background::color::rgb(this->getRandom(begin,end),this->getRandom(begin,end),this->getRandom(begin,end));
+    updateStyle();
+}
+
+void ContainerWidget::setBackGroundRGBRandomAplha(int alpha , int begin, int end )
+{
+    if( begin > end ) begin = end-1;
+    if( begin < 0 ) begin = 0;
+    if( end < begin ) end = begin + 1;
+    if( end > 255 ) end = 255;
+
+    if( alpha > 100 ) alpha = 100;
+    if( alpha < 0 ) alpha = 0;
+
+    double _alpha = static_cast<double>(alpha) / 100;
+
+    mBackGroundImg = Style::background::color::rgba(this->getRandom(begin,end),this->getRandom(begin,end),this->getRandom(begin,end),_alpha);
+    updateStyle();
+
+}
+
+void ContainerWidget::setBorder(StandardColor color)
+{
+    decorationStyle().setBorder(WBorder(BorderStyle::Solid,BorderWidth::Thick,WColor(color)));
+}
+
+void ContainerWidget::setLeftSide(int inpixel)
+{
+    mLeftSpace = "left:"+std::to_string(inpixel)+"px;";
+    this->updateStyle();
+}
+
+void ContainerWidget::setTopSide(int inpixel)
+{
+    mRightSpace = "top:"+std::to_string(inpixel)+"px;";
+    this->updateStyle();
+}
+
+void ContainerWidget::setRightSide(int pixel)
+{
+    mTopSpace = "right:"+std::to_string(pixel)+"px;";
+    updateStyle();
+}
+
+void ContainerWidget::setBottomSide(int pixel)
+{
+    mBottomSpace = "bottom:"+std::to_string(pixel)+"px;";
+    updateStyle();
+}
+
+
+
+mongocxx::collection ContainerWidget::collection(const std::string& collectionname)
+{
+    return mdb->collection(collectionname);
+}
+
+mongocxx::database *ContainerWidget::db() const
+{
+    return mdb;
+}
+
+void ContainerWidget::updateStyle()
+{
+    std::string str = mLeftSpace+mRightSpace+mTopSpace+mBottomSpace
+        +mBackGroundImg;
+    setAttributeValue(Style::style,str);
+}
+
+int ContainerWidget::getRandom(int begin, int end)
+{
+    std::random_device rd;
+    std::map<int, int> hist;
+    std::uniform_int_distribution<int> dist(begin, end);
 }
--- src/containerwidget.h
+++ src/containerwidget.h
@@ -1,12 +1,108 @@
 #ifndef CONTAINERWIDGET_H
 #define CONTAINERWIDGET_H
 
-#include <QtCore/qglobal.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WCssDecorationStyle.h>
+
+#include "bootstrap.h"
+#include "inlinestyle.h"
+
+#include <string>
+
+
+#include "mongocxx.h"
+
+
+using namespace Wt;
 
 class ContainerWidget : public WContainerWidget
 {
 public:
-ContainerWidget();
+    ContainerWidget(mongocxx::database* db_);
+
+///
+/// \brief addBackGroundImage
+/// \param imgpath
+/// Set image to Widget Background
+void setBackGroundImage( const std::string& imgpath );
+
+///
+/// \brief setBackGroundColor
+/// \param color
+/// set color to Background
+void setBackGroundColor( const std::string& color );
+void setBackGroundRGB( int red , int green , int blue );
+void setBackGroundRGBA( int red , int green , int blue , double alpha = 1.0 );
+void setBackGroundRGBRandom( int begin = 0 , int end = 255 );
+void setBackGroundRGBRandomAplha(int alpha = 100 , int begin = 0 , int end = 255 );
+
+///
+/// \brief setBorder
+/// \param color
+/// setborder containerWidget Border with color
+void setBorder(StandardColor color = StandardColor::White );
+
+
+///
+/// \brief setLeftSide
+/// \param inpixel
+/// set Left side Space if Container Aligned in ABSOLUTE|STATIC|FIXED
+void setLeftSide( int inpixel = 0 );
+
+///
+/// \brief setTopSide
+/// \param inpixel
+/// set Top side space if Container Aligned in ABSOLUTE|STATIC|FIXED
+void setTopSide( int inpixel = 0 );
+
+///
+/// \brief setRightSide
+/// \param pixel
+/// set Right side Space if Container Aligned in ABSOLUTE|STATIC|FIXED
+void setRightSide( int pixel = 0 );
+
+///
+/// \brief setBottomSide
+/// \param pixel
+/// set Bottom side Space if Container Aligned in ABSOLUTE|STATIC|FIXED
+void setBottomSide( int pixel = 0 );
+
+///
+/// \brief collection
+/// \param collectionname
+/// \return
+/// get Collectio  of Database
+mongocxx::collection collection(const std::string &collectionname );
+
+
+///
+/// \brief db
+/// \return
+/// get Database pointer indicator
+mongocxx::database* db() const;
+
+private:
+
+    mongocxx::database* mdb;
+
+
+    ///
+    /// \brief setSideSpaces
+    /// Prepare and Set Style Attribute
+    void updateStyle();
+
+    std::string mLeftSpace;
+    std::string mRightSpace;
+    std::string mTopSpace;
+    std::string mBottomSpace;
+    std::string mBackGroundImg; // back ground image
+
+
+    int getRandom( int begin , int end );
+
+
+
 };
 
 #endif // CONTAINERWIDGET_H
