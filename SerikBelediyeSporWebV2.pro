2f7b53c7e4761abea69c451cd6180b9c018d5622
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-28 14:31:25 +0300

    first commit

---
+++ SerikBelediyeSporWebV2.pro
@@ -0,0 +1,45 @@
+TEMPLATE = app
+CONFIG += console c++17
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        main.cpp \
+    mainapplication.cpp
+
+
+
+
+
+win32: LIBS += -L$$PWD/../../Libraries/Wt-4.0.4-msvs2017-x64/lib/ -lwt
+
+INCLUDEPATH += $$PWD/../../Libraries/Wt-4.0.4-msvs2017-x64/include
+DEPENDPATH += $$PWD/../../Libraries/Wt-4.0.4-msvs2017-x64/include
+
+
+
+
+win32: LIBS += -L$$PWD/../../Libraries/Wt-4.0.4-msvs2017-x64/lib/ -lwthttp
+
+INCLUDEPATH += $$PWD/../../Libraries/Wt-4.0.4-msvs2017-x64/include
+DEPENDPATH += $$PWD/../../Libraries/Wt-4.0.4-msvs2017-x64/include
+
+HEADERS += \
+    mainapplication.h \
+    resource.h
+
+
+
+
+win32: LIBS += -L$$PWD/mongo/lib/ -lbsoncxx
+
+INCLUDEPATH += $$PWD/mongo/include/bsoncxx/v_noabi
+DEPENDPATH += $$PWD/mongo/include/bsoncxx/v_noabi
+
+
+
+
+win32: LIBS += -L$$PWD/mongo/lib/ -lmongocxx
+
+INCLUDEPATH += $$PWD/mongo/include
+DEPENDPATH += $$PWD/mongo/include
---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <mongocxx/instance.hpp>
+#include <Wt/WApplication.h>
+#include <Wt/WEnvironment.h>
+
+#include <Wt/WResource.h>
+#include <Wt/WServer.h>
+#include "resource.h"
+
+#include "mainapplication.h"
+
+
+
+
+using namespace std;
+using namespace Wt;
+
+
+
+std::unique_ptr<WApplication> createApplication(const WEnvironment& env)
+{
+    auto app = Wt::cpp14::make_unique<MainApplication>(env);
+    app->setTitle("Serik Belediye Spor Environment @ " + env.hostName());
+    return std::move(app);
+}
+
+
+
+int main()
+{
+    cout << "Start up Serik Belediye Spor Web Application 2018" << endl;
+
+    mongocxx::instance ins{};
+
+
+    int argc = 6;
+    auto argv = std::unique_ptr<char*[]>(new char*[argc]);
+
+    //--docroot docroot --http-address 192.168.0.31 --http-port 8085 --approot approot --config approot/wt_config.xml
+    argv[0] =  (char*)"SerikBelediyeSporWebV2";
+    argv[1] =  (char*)"--http-address=192.168.0.31";
+    argv[2] =  (char*)"--http-port=8085";
+    argv[3] =  (char*)"--deploy-path=/";
+    argv[4] =  (char*)"--docroot=docroot";
+    argv[5] =  (char*)"--approot=approot";
+//    argv[6] =  (char*)"--config=approot/wt_config.xml";
+
+
+
+
+
+    try {
+
+        Wt::WServer server{argc, argv.get(), WTHTTP_CONFIGURATION};
+
+        Resource resource;
+
+//        server.addResource(&resource, "/");
+//        server.addResource(&resource, "/${test}");
+        server.addResource(&resource, "/users");
+        server.addResource(&resource, "/users/${user}");
+        server.addResource(&resource, "/users/${user}/posts");
+        server.addResource(&resource, "/users/${user}/posts/${post}");
+
+        server.addEntryPoint(Wt::EntryPointType::Application, createApplication );
+
+
+
+
+        //        server.addEntryPoint(EntryPointType::Application, [&](const Wt::WEnvironment &env) {
+        //            return Wt::cpp14::make_unique<HelloApplication>(env,);
+        //        } );
+
+
+
+
+        server.run();
+
+
+
+    } catch (const Wt::WServer::Exception &e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    } catch (const std::exception &e) {
+        std::cerr << "exception: " << e.what() << '\n';
+        return 1;
+    }
+
+
+    return 0;
+}
