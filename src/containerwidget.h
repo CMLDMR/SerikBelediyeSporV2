f3b1ed4408d13faa72529ef87982b7bdcc452bfc
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-28 18:01:42 +0300

    ContainerWidget added.

---
+++ src/containerwidget.h
@@ -0,0 +1,12 @@
+#ifndef CONTAINERWIDGET_H
+#define CONTAINERWIDGET_H
+
+#include <QtCore/qglobal.h>
+
+class ContainerWidget : public WContainerWidget
+{
+public:
+ContainerWidget();
+};
+
+#endif // CONTAINERWIDGET_H
